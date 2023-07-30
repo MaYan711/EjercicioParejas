053742d00674c52d483d559e9947f3978687d680
= <=>
2023-07-30 17:59:23 -0600

    Se agrega

---
+++ PracticaCortaGit/src/main/java/blackjack/mano.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blackjack;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author LL829
+ */
+public class mano {
+    ArrayList<carta> arr = new ArrayList<carta>();
+    int positin = 0;
+    
+    public void addcarta(carta c){
+        arr.add(c);
+    }
+    
+    public String toString(){
+        String mano ="";
+        for (int i=0; i <arr.size(); i++)
+        mano += arr.get(i).toString() + "";
+        return mano;
+    }
+    public int getValue(){
+        int val = 0;
+        boolean ace= false;
+        for (int i=0; i <arr.size(); i++){
+        carta tmpcarta = arr.get(i);
+        int cartaValue = tmpcarta.getValue();
+        if (cartaValue>10)
+            cartaValue=10;
+        if (cartaValue==1)
+            ace = true;
+        val += cartaValue;
+        
+    }
+        if (ace ==true && val +10 <=21)
+            val = val +10;
+        return val;
+    }
+}
---
+++ PracticaCortaGit/src/main/java/blackjack/mazo.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blackjack;
+
+
+import java.util.Arrays;
+import java.util.Collections;
+/**
+ *
+ * @author LL829
+ */
+public class mazo {
+    carta cartas [] = new carta[52];
+    int position;
+    
+    public void createCards(){
+        String symbols = "123456789JQKA";
+        String types = "SCDH";
+        int index =0;
+        
+        for (int i=0; i < types.length(); i++){
+            for (int j=0; j < symbols.length(); j++);{
+            carta tempcarta = new carta();
+            tempcarta.setSymbol(symbols.charAt(j));
+            tempcarta.setType(types.charAt(i));
+            this.cartas[index] = tempcarta;
+            index++;
+            
+        }
+        }
+        shufflecartas();
+        
+    }
+    private void shufflecartas(){
+        Collections.shuffle(Arrays.asList(this.cartas));
+        
+    }
+    public carta getcarta(){
+        if (position ==51){
+            System.out.println("Se acabaron las cartas");
+            return null;
+        }
+        carta tempcarta = this.cartas[position];
+        position++;
+        return tempcarta;
+    }
+}
