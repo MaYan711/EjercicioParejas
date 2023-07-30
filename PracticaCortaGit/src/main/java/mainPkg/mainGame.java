3bd67be42ab1b750b65d7b9bf70b1a10d1b72a50
SirBonn <byronvasquez21@gmail.com>
2023-07-29 18:21:34 -0600

    creacion de manu principal funcional

--- PracticaCortaGit/src/main/java/mainPkg/mainGame.java
+++ PracticaCortaGit/src/main/java/mainPkg/mainGame.java
@@ -5,17 +5,56 @@
  */
 package mainPkg;
 
+import org.w3c.dom.Text;
+
+import utils.*;
+
 /**
  *
  * @author sirbon
  */
 public class mainGame {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("Hello world");// TODO code application logic here
+
+        mainGame newGame = new mainGame();
     }
-    
+
+    public mainGame() {
+        mainMenu();
+    }
+
+    private void mainMenu() {
+
+        boolean exit = false;
+
+        do {
+            TextArt.showTittle();
+            TextArt.showMenuTittle();
+            System.out.println("\n\n1. Jugar ahorcado");
+            System.out.println("2. Jugar baloncesto");
+            System.out.println("3. Jugar cartas");
+            System.out.println("4. Ver instrucciones");
+            System.out.println("0. Salir");
+
+            switch (ConsoleData.getInt("Ingrese la opcion seleccionada: ", false)) {
+                case 0:
+                    exit = true;
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+
+            default:
+                TextArt.showErrorMsg();
+                ConsoleData.waitKey(TextArt.BG_RED + " --> Pulse una tecla para continuar <--" +TextArt.RESET);
+            }
+
+        } while (exit == false);
+
+    }
+
 }
